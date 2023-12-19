90949fb1a84cd356917f2af22d24c6721195edff
eye <eyenathaya5@gmail.com>
2023-12-19 16:35:00 +0700

    Filled out lab10_2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,7 +8,14 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
+
+	string textline ;
+	dest << "-------------------- BOOM ---------------------" << endl;
+	while(getline(source,textline)){
+		dest << textline << endl;
+	}
+	dest << "-------------------- HA!! ---------------------" << endl;
+
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
